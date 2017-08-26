818cf3ac3b2c07fc3b99dcb3e2fae9680ec99b4b
Chris Neely <chris-neely@users.noreply.github.com>
2017-08-25 22:28:37 -0500

    change outfile to path

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
   
 ### get-oktaGroupMembers.ps1  
 **Description:** PowerShell script to export Okta group membership to a CSV file  
-**Example:** .\get-oktaGroupMembers.ps1 -org "tenant.okta.com" -gid "0000" -api_token "0000" -outfile "c:\scripts\file.csv"
+**Example:** .\get-oktaGroupMembers.ps1 -org "tenant.okta.com" -gid "0000" -api_token "0000" -path "c:\scripts\file.csv"
 
 ### put-oktaGroupMembers.ps1
 **Description:** PowerShell script for adding a CSV file of user email addresses to an Okta group  
--- get-oktaGroupMembers.ps1
+++ get-oktaGroupMembers.ps1
@@ -4,7 +4,7 @@ Purpose: Script for exporting Okta group membership to a csv file
 Author: Chris Neely
 E-mail: chris@neely.pro
 Notes: Requires PowerShell3.0 or later
-Example: .\get-oktaGroupMembers.ps1 -org "tenant.okta.com" -gid "0000000000" -api_token "0000000000" -outfile "c:\scripts\groupname.csv"
+Example: .\get-oktaGroupMembers.ps1 -org "tenant.okta.com" -gid "0000" -api_token "0000" -path "c:\scripts\groupname.csv"
 #>
 
 #requires -version 3.0
@@ -13,7 +13,7 @@ param(
     [Parameter(Mandatory=$true)]$org, # Your tentant prefix - Ex. tenant.okta.com
     [Parameter(Mandatory=$true)]$gid, # The group ID for the group you want to export - Ex. https://tenant-admin.okta.com/admin/group/00000000000000000000
     [Parameter(Mandatory=$true)]$api_token, # Your API Token.  You can generate this from Admin - Security - API
-    [Parameter(Mandatory=$true)]$outfile # The path and file name for the resulting CSV file
+    [Parameter(Mandatory=$true)]$path # The path and file name for the resulting CSV file
     )
 
 ### Define $allusers as empty array
@@ -39,4 +39,4 @@ $activeUsers = $allusers | Where-Object { $_.status -ne "DEPROVISIONED" }
 ### Select the user profile properties we want and export to CSV
 $activeUsers | Select-Object -ExpandProperty profile | 
     Select-Object -Property email, displayName, primaryPhone, mobilePhone, organization, department | 
-    Export-Csv -Path $outfile -NoTypeInformation
+    Export-Csv -Path $path -NoTypeInformation
